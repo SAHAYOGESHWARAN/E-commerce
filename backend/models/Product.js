73e67344a9ff15b095163bf63225b8956974f5c8
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-20 11:13:06 +0530

    ECP

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  image: { type: String },
+  description: { type: String },
+  category: { type: String },
+  stock: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
