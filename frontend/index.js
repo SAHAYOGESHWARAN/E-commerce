a074325b48a3f2b7569a7794ff65dfbab062b1fb
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-08-20 11:10:56 +0530

    Ecp

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; // Ensure this path is correct
+import App from './App';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.render(
